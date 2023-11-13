549ff3e7afdeabfe50dc8df54596fb034010a26b
s3632442 <s363244@student.rmit.edu.au>
2023-11-13 20:28:19 +1100

    1.3 in action

--- lambda_function.py
+++ lambda_function.py
@@ -1,34 +1,47 @@
+import boto3
+from PIL import Image
+import imagehash
 import json
+
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     # Example input event
     # {
-    #     "id": "123456",
-    #     "metrics": {
-    #         "humans or animals": "animals",
-    #         "natural or arteficial": "natural",
-    #         "me or them": "them",
-    #         "sustainability or now": "now"
-    #     }
+    #     "approved_bucket": "approved-vehicle-images",
+    #     "pending_bucket": "pending-vehicle-images",
+    #     "image_key_approved": "image1.jpg",
+    #     "image_key_pending": "image2.jpg"
     # }
 
-    # Extract metrics and ID from the input event
-    metrics = event.get("metrics", {})
-    item_id = event.get("id")
+    # Extract input parameters from the event
+    approved_bucket = event.get("approved_bucket")
+    pending_bucket = event.get("pending_bucket")
+    image_key_approved = event.get("image_key_approved")
+    image_key_pending = event.get("image_key_pending")
 
-    # Sort the metrics by their matching numeric values in reverse order
-    sorted_metrics = sorted(metrics.items(), key=lambda x: int(x[1]), reverse=False)
+    # Download images from S3
+    approved_image = download_image(approved_bucket, image_key_approved)
+    pending_image = download_image(pending_bucket, image_key_pending)
 
-    # Create a JSON response with text values ordered by numeric values in reverse
+    # Calculate perceptual hash for both images
+    hash_approved = imagehash.average_hash(approved_image)
+    hash_pending = imagehash.average_hash(pending_image)
+
+    # Compare hashes to check if images are the same
+    are_images_same = hash_approved == hash_pending
+
     response = {
-        "id": item_id,
-        "ordered_categories": [
-            {"category": category, "text_value": text_value} for category, text_value in sorted_metrics
-        ]
+        "are_images_same": are_images_same
     }
 
     return {
         "statusCode": 200,
         "body": json.dumps(response)
     }
+
+def download_image(bucket, key):
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    image_data = obj['Body'].read()
+    return Image.open(io.BytesIO(image_data))
 
